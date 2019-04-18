7ba39698b861bee286db13d93faeac9ec1954612
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-18 21:21:43 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -15,7 +15,7 @@ def third_challenge
         status: "alive"
         },
       :hero_friends => [
-        {name: "Benvolio", age: "17", attitude: "worried"}
+        {name: "Benvolio", age: "17", attitude: "worried"},
         {name: "Mercutio", age: "18", attitude: "hot=headed"}
         ]
    }, 
@@ -34,7 +34,7 @@ def third_challenge
         status: "alive"
         },
       :heroine_friends => [
-        {name: "Steven", age: "30", attitude: "confused"}
+        {name: "Steven", age: "30", attitude: "confused"},
         {name: "Nurse", age: "44", attitude: "worried"}
         ]
    }
